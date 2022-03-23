377a5bc0cd5c2a3faa19509d854f750061dd8dc9
William Owalla <william.owalla@student.moringaschool.com>
2022-03-23 21:01:04 +0300

    ts: fail OO form validation using

--- JS/script.js
+++ JS/script.js
@@ -10,46 +10,46 @@ $(document).ready(function () {
 
 // OO form validation
 
-;(function (window, undefined) {
-    const Verifier ={
-        constructor: function (form, config) {
-            this._dataForm = form;
-            this._data = config.fields || {};
+// ;(function (window, undefined) {
+//     const Verifier ={
+//         constructor: function (form, config) {
+//             this._dataForm = form;
+//             this._data = config.fields || {};
 
-            this.init();
-        },
-        init: function () {
-            this.addFormListener();
-        },
-    addFormListener: function () {
-            let formSelector = this._dataForm, dataForm = document.querySelector(formSelector);
-            dataForm.addEventListener("submit", this.validate.bind(this), false);
-        },
-        validate: function (e) {
-            let dataFields = this._data;
-            for(var field in dataFields){
-                givenData = document.querySelector(field), givenDataVal = givenData.value;
-                if (dataFields[field].require || givenDataVal === "" || givenDataVal.length > dataFields[field].maxlength) {
-                    givenData.classList.alert("invalid");
-                }
-            }
-            e.preventDefault();
-        }
-    }
+//             this.init();
+//         },
+//         init: function () {
+//             this.addFormListener();
+//         },
+//     addFormListener: function () {
+//             let formSelector = this._dataForm, dataForm = document.querySelector(formSelector);
+//             dataForm.addEventListener("submit", this.validate.bind(this), false);
+//         },
+//         validate: function (e) {
+//             let dataFields = this._data;
+//             for(var field in dataFields){
+//                 givenData = document.querySelector(field), givenDataVal = givenData.value;
+//                 if (dataFields[field].require || givenDataVal === "" || givenDataVal.length > dataFields[field].maxlength) {
+//                     givenData.classList.add("invalid");
+//                 }
+//             }
+//             e.preventDefault();
+//         }
+//     }
 
-    let inputForm = Object.create(Verifier);
-    inputForm.constructor("#form", {
-        fields: {
-            "#name": {
-                required: true,
-                maxlength: 20
-            },
-            "#message": {
-                maxlength: 50
-            }
-        }
-    })
+//     let inputForm = Object.create(Verifier);
+//     inputForm.constructor("#form", {
+//         fields: {
+//             "#name": {
+//                 required: true,
+//                 maxlength: 20
+//             },
+//             "#message": {
+//                 maxlength: 50
+//             }
+//         }
+//     })
 
-})(window, undefined);
+// })(window, undefined);
 
     
