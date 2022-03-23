6444133441c85af61b5a71ae50bfbdb9a5361fe4
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-03-24 02:06:04 +0300

    modified files

--- JS/service.js
+++ JS/service.js
@@ -1,1 +1,16 @@
-
+  $(".text").click(function () {
+        $(".text2").toggle();
+    });
+  $(".comfy").click(function () {
+        $(".comfy2").toggle();
+    }); 
+    $(".book").click(function () {
+        $(".book2").toggle();
+    });  
+    $(".wifi").click(function () {
+        $(".wifi2").toggle();
+    });  
+   
+//   $(".text").click(function(){
+//         alert("The paragraph was clicked.");
+//       });
