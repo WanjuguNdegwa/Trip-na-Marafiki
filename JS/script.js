5d5040fa1870be9788bc0aecf56e0eb2d9ce96d2
William Owalla <william.owalla@student.moringaschool.com>
2022-03-23 20:56:41 +0300

    ts: testing 00 form validation

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,49 @@ $(document).ready(function () {
         $(".pop").css("fontSize", size);
     });
 });
+
+// OO form validation
+
+;(function (window, undefined) {
+    const Verifier ={
+        constructor: function (form, config) {
+            this._dataForm = form;
+            this._data = config.fields || {};
+
+            this.init();
+        },
+        init: function () {
+            this.addFormListener();
+        },
+    addFormListener: function () {
+            let formSelector = this._dataForm, dataForm = document.querySelector(formSelector);
+            dataForm.addEventListener("submit", this.validate.bind(this), false);
+        },
+        validate: function (e) {
+            let dataFields = this._data;
+            for(var field in dataFields){
+                givenData = document.querySelector(field), givenDataVal = givenData.value;
+                if (dataFields[field].require || givenDataVal === "" || givenDataVal.length > dataFields[field].maxlength) {
+                    givenData.classList.alert("invalid");
+                }
+            }
+            e.preventDefault();
+        }
+    }
+
+    let inputForm = Object.create(Verifier);
+    inputForm.constructor("#form", {
+        fields: {
+            "#name": {
+                required: true,
+                maxlength: 20
+            },
+            "#message": {
+                maxlength: 50
+            }
+        }
+    })
+
+})(window, undefined);
+
+    
