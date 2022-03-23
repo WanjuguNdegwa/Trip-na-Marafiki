c2f75407ef05cc3044cbc48bb1e8fc5554f6b707
chepceline <chepceline25@gmail.com>
2022-03-23 15:14:47 +0300

    add document.ready defined variables

--- JS/signup.js
+++ JS/signup.js
@@ -1,52 +1,18 @@
 $(document).ready(function(){
 
-    $("#forms").submit(function(){
-        return formVal();
+    $("#forms").submit(function(event){
+        event.preventDefault();
+        // console.log("submit");
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+        var confirmpassword = $("#confirm-password").val
+
+        var regex2 = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]{2,10})(\.[a-zA-Z]{2,7})?$/
+        if (email === ""){
+            $("#emailval")
+        }
     });
   })
-    const formData = document.querySelector("#forms");
-  
-    formData.addEventListener("submit",formVal);
-  
-    function formVal(){    
-      var email = $("#email").val();
-      var password = $("#password").val();
-      var confirmpassword = $("#confirmpassword").val();
-  
-      var regex2 = /^([_\-\.%0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]{2,10})(\.[a-zA-Z]{2,7})?$/
-      if(email === ""){
-          $("#emailval").show();
-          return false
-      }else{       
-         if (!regex2.test(email)){
-          $("#emailval2").show();
-          return false;
-         }
-      }
-      var regex3 = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#\._%()!])[a-zA-Z0-9@#\._%()!]+$/
-      if(password === ""){
-          $("#passval").show();
-          return false
-      }else if (!regex3.test(password)){
-          $("#passval2").show();
-          return false;
-      }else if (password.length < 8 || password.length > 15){
-          $("#passval3").show();
-          return false;
-      }
-      if(confirmpassword === ""){
-        $("#conpassval").show();
-        return false
-    }else if (confirmpassword !== password){
-        $("#conpassval2").show();
-        return false;
-    }else{
-        alert ("successful");
-    }
-    return true;
     
-  }
-    
-  
-  
-  
+      
